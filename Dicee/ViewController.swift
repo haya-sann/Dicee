cffc7462c31280d9bf5c21e1d26b93ce96d5aa78
Nobuo Hayashi <haya.biz@gmail.com>
2018-03-18 02:16:05 +0900

    Using delayWithSeconds

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var bonacinno: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +35,7 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         updateDiceImages()
-  
+
     }
     func updateDiceImages(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
@@ -41,14 +43,57 @@ class ViewController: UIViewController {
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        print(randomDiceIndex2)
+        
+        // イメージビューを画面にセットする。
+        self.view.addSubview(diceImageView1)
+        self.view.addSubview(diceImageView2)
+        
+        print(randomDiceIndex1, randomDiceIndex2)
+    }
+    
+    func slowDisplay() {
+        // to run something in 10 seconds
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+            self.updateDiceImages()
+        }
+        
+    }
+    
+    
+    func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            completion()
+        }
     }
     
     override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        for _ in 1...5 {
-            updateDiceImages()
-            sleep(1)
-            }
+//        for _ in 1...5 {
+//            updateDiceImages()
+//            sleep(1)
+//            }
+
+        delayWithSeconds(5){
+            self.updateDiceImages()
         }
+        
+        delayWithSeconds(5){
+            self.updateDiceImages()
+        }
+        
+        delayWithSeconds(5){
+            self.updateDiceImages()
+        }
+        
+        delayWithSeconds(5){
+            self.updateDiceImages()
+        }
+        
+        delayWithSeconds(5){
+            self.updateDiceImages()
+        }
+        
+    }
+    
 
 }
